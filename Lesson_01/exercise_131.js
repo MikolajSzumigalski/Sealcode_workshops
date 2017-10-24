ef7f401ea4e65e9099b73bb6da21af84de06bfa3
MikolajSzumigalski <m.szumigalski@op.pl>
2017-10-24 13:01:22 +0200

    nowe zadanie

---
+++ Lesson_01/exercise_131.js
@@ -0,0 +1,28 @@
+function Figura(nazwa){
+    this.nazwa = nazwa;
+}
+
+function Czworokat(typczworokata, dlugoscibokow){
+    this.typczworokata = typczworokata;
+    this.dlugoscibokow = dlugoscibokow;
+    this.podajpole(){
+        
+    }
+}
+
+function Prostokat(){
+    this.podajpole = function(){
+        
+    }
+    this.podajobwod = function(){
+        
+    }
+}
+
+
+
+x = new Protokat();
+console.log(x.nazwa);
+console.log(x.typ_czworokata);
+console.log(x.podajpole);
+console.log(x.podajObwod);
