4d3324eef0258d59938a42502bc58d293372e8d0
MkolajSzumigalski <m.szumigalski@op.pl>
2017-11-21 10:30:20 +0100

    prdom

---
+++ Lesson_02/homework_task_01.js
@@ -0,0 +1,7 @@
+$('aside h2').append('!');
+$('ul li:last').after('<li id="new-id">Nowy element</li>');
+$('footer').remove();
+$('h1.header1').attr('class','header-new');
+$('ul li:last').css({'color': 'red', 'font-family': "serif", 'line-height': '15px'});
+$('ul').css("background-color", "#0aff0a");
+$('aside h2').append('!');
